6ceef1626bf93beeddd1bad3297271e658bf62ca
julius.steude <julius.steude@stud.h-da.de>
2025-06-09 14:25:41 +0200

    -Refactor naming for better clarity -Severely improve  performance of inbound function -Replace sleep() with curses-based delay

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 
 from decimal import ROUND_UP
-from email.policy import default
 import time
-import os
 import random
 from enum import Enum
 import curses
-import math
 import toml
 
-symbolType = Enum("symbolType", ["Lightning", "Rain", "Snow"])
+particleType = Enum("symbolType", ["Lightning", "Rain", "Snow"])
 
 
 
-class Symbol:
+class Particle:
 
-    def __init__(self, x:int, y:int, char, type:symbolType, temperatur:int):
+    def __init__(self, x:int, y:int, symbol:str, type:particleType, temperatur:int):
         self.x = x
         self.y = y
         self.velocityX = 0.0
         self.velocityY = 0.0
-        self.char = char
-        self.type = type
+        self.symbol = symbol
+        self.particleType = type
         self.temperatur = temperatur
 
 
@@ -36,32 +33,30 @@ class Physics:
         self.gravitation = gravitation
     
     
-    def applyWind(self, symbol:Symbol):
+    def applyWind(self, symbol:Particle):
         symbol.velocityX += self.wind
     
-    def applyGravitation(self, symbol:Symbol):
-        match symbol.type:
-            case symbolType.Rain:        
+    def applyGravitation(self, symbol:Particle):
+        match symbol.particleType:
+            case particleType.Rain:        
                 symbol.velocityX += self.wind
                 symbol.velocityY += self.gravitation
-            case symbolType.Snow:
+            case particleType.Snow:
                 symbol.velocityX += (self.wind *2.5)
                 symbol.velocityY += (self.gravitation/10)
                     
-        #symbol.x += math.ceil(symbol.velocityX)
         symbol.x += int(max(-1, min(symbol.velocityX, 1)))
-
         symbol.y += int(max(-1.4, min(symbol.velocityY, 1.4)))
         
 
-    def applyTemperatur(self, symbol: Symbol):
+    def applyTemperatur(self, symbol: Particle):
         if symbol.temperatur <=0:
-            symbol.char ='❉'
-            symbol.type = symbolType.Snow
+            symbol.symbol ='❉'
+            symbol.particleType = particleType.Snow
 
-    def applyPhysics(self, symbol:Symbol):
-        match symbol.type:
-            case symbolType.Lightning:
+    def applyPhysics(self, symbol:Particle):
+        match symbol.particleType:
+            case particleType.Lightning:
                 pass
             case default:
                 self.applyTemperatur(symbol)
@@ -72,10 +67,9 @@ class Physics:
 
 
 class MainLoop:
-    def __init__(self,fps:int,stdscr):
-
+    def __init__(self,stdscr,configFile:str):
         self.debug = True
-        self.config = toml.load("config.toml")
+        self.config = toml.load(configFile)
         self.stdscr = stdscr
         curses.curs_set(0)
         stdscr.nodelay(True)
@@ -98,16 +92,19 @@ class MainLoop:
 
         self.height, self.width = stdscr.getmaxyx()
     def inbound(self):
-        for elements in self.symbolList:
-            if elements.x > self.width:
-                elements.x = elements.x % self.width
-            self.symbolList = [s for s in self.symbolList if 0 <= s.y < self.height]
+        new_symbol_list = []
+        for element in self.symbolList:
+            if element.y < self.height:
+                if element.x > self.width:
+                    element.x %= self.width
+                new_symbol_list.append(element)        
+        self.symbolList = new_symbol_list
     
     def spawnDrops(self,):
         for i in range(self.raindropCount):
             x = random.randint(0, self.width - 1)
             char = random.choices(['|', '¦','╿'], [5,5,1])[0]
-            symbol = Symbol(x, 0, char, symbolType.Rain, random.randint(0,1))
+            symbol = Particle(x, 0, char, particleType.Rain, random.randint(0,1))
             
             symbol.velocityY = random.uniform(0.05, 0.15)
             self.symbolList.append(symbol)
@@ -121,14 +118,14 @@ class MainLoop:
 
 
         for i in range(0, self.height):
-            self.symbolList.append(Symbol(random.randint(x-3,x+3 ),y, random.choice(symbols), symbolType.Lightning, 10))
+            self.symbolList.append(Particle(random.randint(x-3,x+3 ),y, random.choice(symbols), particleType.Lightning, 10))
             y+=1
 
     def thunderclear(self):
          if self.thundertimer >0:
                 self.thundertimer -=1
                 if self.thundertimer == 0:
-                    self.symbolList = [s for s in self.symbolList if s.type != symbolType.Lightning]
+                    self.symbolList = [s for s in self.symbolList if s.particleType != particleType.Lightning]
                     self.stdscr.bkgd(' ', curses.color_pair(0))
                     curses.init_pair(1, curses.COLOR_BLUE,   -1)  
                     curses.init_pair(2, curses.COLOR_WHITE,  -1)  
@@ -155,16 +152,16 @@ class MainLoop:
                 self.stdscr.addstr(self.height-1, 0, self.debugstring[:self.width-1], curses.A_REVERSE)
             for s in self.symbolList:
                 y, x = int(s.y), int(s.x)
-                match s.type:
-                    case symbolType.Rain:
+                match s.particleType:
+                    case particleType.Rain:
                         attr = curses.color_pair(1)
-                    case symbolType.Snow:      
+                    case particleType.Snow:      
                         attr = curses.color_pair(2)
                     case default:                                 
                         attr = curses.color_pair(3)
 
                 try:
-                    self.stdscr.addstr(y-1, x, s.char, attr)
+                    self.stdscr.addstr(y-1, x, s.symbol, attr)
                 except curses.error:
                     pass
 
@@ -188,7 +185,7 @@ class MainLoop:
 
 
                     for i in range(0, self.height):
-                        self.symbolList.append(Symbol(random.randint(x-2,x+2 ),y, random.choice(symbols), symbolType.Lightning, 10))
+                        self.symbolList.append(Particle(random.randint(x-2,x+2 ),y, random.choice(symbols), particleType.Lightning, 10))
                         
                         y+=1
                 case 43:
@@ -203,6 +200,7 @@ class MainLoop:
 
     def loop(self):
         while(True):
+
             t = time.time()
             if not self.handle_input():
                  break
@@ -214,10 +212,12 @@ class MainLoop:
             self.inbound()
 
             self.draw()
-            time.sleep(max(0,(1/self.fps)- (time.time()-t)))
+
+            curses.delay_output(int(max(0, (1 / self.fps) - (time.time() - t)) * 1000))
             self.debugstring = f"{str(round(1/(time.time()- t)))} wind: {str(round(self.physics.wind, 2))} gravitation: {str(round(self.physics.gravitation, 2))} Symbole: {len(self.symbolList)}"
+
 def start_engine(stdscr):
-    engine = MainLoop(fps=30, stdscr=stdscr)
+    engine = MainLoop(stdscr, "config.toml")
     engine.loop()
 
 if __name__ == '__main__':
